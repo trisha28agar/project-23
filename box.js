6247aec4a2cb8bfae2d99305cce75dbb70b9909b
trisha28agar <73343319+trisha28agar@users.noreply.github.com>
2021-02-04 16:56:04 +0530

    uploaded changes

---
+++ box.js
@@ -0,0 +1,20 @@
+class Box{
+    constructor(x,y,width,height,color) {
+        var options = {
+            isStatic: true
+        }
+
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        this.color = color;
+
+        World.add(world,this.body);
+    }
+
+    display() {
+        rectMode(CENTER);
+        rect(this.body.position.x,this.body.position.y,this.width,this.height);
+        
+    }
+}
